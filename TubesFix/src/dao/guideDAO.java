e19e0e8506bee31854b487923b2baf852362ee8e
thompluk <thomasdany38@gmail.com>
2022-06-04 17:14:41 +0700

    dao guide dan view guide

---
+++ TubesFix/src/dao/guideDAO.java
@@ -0,0 +1,157 @@
+
+package dao;
+
+import connection.DbConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import model.Guide;
+
+public class guideDAO {
+    private DbConnection dbCon = new DbConnection();
+    private Connection con;
+    
+    public void insertGuide(Guide g) {
+        con = dbCon.makeConnection();
+        
+        String sql = "INSERT INTO tour_guide(id_Guide, nama_Guide, alamat, umur, no_telp)" // bakal dicek dulu
+                + "VALUES ('" +g.getId_Guide()+ "', '" + g.getId_Guide()+ "','" + g.getAlamat_Guide()+ "', '"
+                + g.getUmur_Guide() + "', '" + g.getTelp_Guide() + "')";
+        
+        System.out.println("Adding Guide...");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Added " +result + " Guide");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Errror adding Guide...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+    
+    public void updateGuide(Guide g){
+        con = dbCon.makeConnection();
+        
+        String sql = "UPDATE customer SET nama_Guide = '" + g.getNama_Guide()
+                + "', alamat = '" + g.getAlamat_Guide()
+                + "', umur = '" + g.getUmur_Guide()
+                + "' WHERE id = '" + g.getId_Guide() + "'";
+        System.out.println("Editing Guide...");
+        
+        try {
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Edited " + result + " Guide " + g.getId_Guide());
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Error Updating Guide...");
+            System.out.println(e);
+        }
+        
+        dbCon.closeConnection();
+    }
+    
+    public List<Guide> showGuideBySearch(String query){
+        con = dbCon.makeConnection();
+        
+        String sql = "SELECT g.* FROM tour_guide as g WHERE (g.id_Guide LIKE"
+                + "'%" + query + "%'"
+                + "OR g.nama_Guide LIKE '%" + query + "%'"
+                + "OR g.alamat LIKE '%" + query +"%'"
+                + "OR g.umur LIKE '%" + query + "%'"
+                + "OR g.no_telp LIKE '%" + query + "%')";
+
+        System.out.println("Mengambil data Guide...");
+        
+        List<Guide> list = new ArrayList();
+        
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if (rs != null){
+                while (rs.next()){
+                        Guide g = new Guide(
+                            rs.getString("g.id_Guide"),
+                            rs.getString("g.nama_Guide"),
+                            rs.getString("g.alamat"),
+                            Integer.parseInt(rs.getString("g.umur")),
+                            rs.getString("g.no_telp")
+                    );  
+                    list.add(g);
+                   }
+                }
+                rs.close();
+                statement.close();
+            } catch (Exception e){
+                    System.out.println("Error reading database ...");
+                    System.out.println(e);
+                    }
+            dbCon.closeConnection();
+            
+            return list;
+    }
+    
+    public void deleteGuide(String id){
+        
+        con = dbCon.makeConnection();
+        
+        String sql = "DELETE FROM tour_guide WHERE id_Guide = " + id + "";
+                
+        System.out.println("Deleting Guide...");
+        
+        try {
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Deleted " + result + " Guide " + id);
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Error Deleting Guide...");
+            System.out.println(e);
+        }
+        
+        dbCon.closeConnection();
+    }  
+    
+    public List<Guide> showGuide(){
+        con = dbCon.makeConnection();
+        
+        String sql = "SELECT * FROM tour_guide";
+        System.out.println("Mengambil data guide...");
+        
+        List<Guide> list = new ArrayList();
+        
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if (rs != null){
+                while (rs.next()){
+                        Guide g = new Guide(
+                            rs.getString("g.id_Guide"),
+                            rs.getString("g.nama_Guide"),
+                            rs.getString("g.alamat"),
+                            Integer.parseInt(rs.getString("g.umur")),
+                            rs.getString("g.no_Telp")
+                    );
+                    
+                    list.add(g);
+                    }
+                }
+                rs.close();
+                statement.close();
+            } catch (Exception e){
+                    System.out.println("Error reading database...");
+                    System.out.println(e);
+                    }
+        
+            dbCon.closeConnection();
+            
+            return list;
+        }
+}
--- TubesFix/src/view/GuideView.java
+++ TubesFix/src/view/GuideView.java
@@ -58,6 +58,8 @@ public class GuideView extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         teleponInput = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
+        searchInput = new javax.swing.JTextField();
+        searchBtn = new javax.swing.JButton();
 
         jInternalFrame1.setVisible(true);
 
@@ -343,6 +345,8 @@ public class GuideView extends javax.swing.JFrame {
 
         jLabel5.setText("Telepon");
 
+        searchBtn.setText("Cari");
+
         javax.swing.GroupLayout jPanel14Layout = new javax.swing.GroupLayout(jPanel14);
         jPanel14.setLayout(jPanel14Layout);
         jPanel14Layout.setHorizontalGroup(
@@ -350,6 +354,7 @@ public class GuideView extends javax.swing.JFrame {
             .addGroup(jPanel14Layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addGroup(jPanel14Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5)
                     .addComponent(teleponInput, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4)
@@ -358,14 +363,22 @@ public class GuideView extends javax.swing.JFrame {
                     .addComponent(alamatInput, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
                     .addComponent(namaInput, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1)
-                    .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel1))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel14Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(searchInput, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(19, 19, 19)
+                .addComponent(searchBtn)
+                .addContainerGap())
         );
         jPanel14Layout.setVerticalGroup(
             jPanel14Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel14Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
+                .addGroup(jPanel14Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(searchBtn))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -501,6 +514,8 @@ public class GuideView extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField namaInput;
     private javax.swing.JButton saveBtn;
+    private javax.swing.JButton searchBtn;
+    private javax.swing.JTextField searchInput;
     private javax.swing.JTextField teleponInput;
     private javax.swing.JTextField umurInput;
     // End of variables declaration//GEN-END:variables
