53d9d14a97246dbed0104408057aefafb9d7b1cb
valentVasti <valentvasti2114@gmail.com>
2022-06-07 19:45:41 +0700

    nambah motorDAO sama mobilDAO

--- TubesFix/src/dao/mobilDAO.java
+++ TubesFix/src/dao/mobilDAO.java
@@ -10,41 +10,39 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import model.Customer;
-import model.Kendaraan_Motor;
 import model.Kendaraan;
+import model.Kendaraan_Mobil;
 
-public class motorDAO {
+public class mobilDAO {
     private DbConnection dbCon = new DbConnection();
     private Connection con;
     
-    public void insertMotor(Kendaraan_Motor m) {
+    public void insertMotor(Kendaraan_Mobil m) {
         con = dbCon.makeConnection();
         
-        String sql = "INSERT INTO motor(id_Kendaraan, jenis_Helm, jenis_Stang, jenis, nama_Kendaraan, platNo, merk, cc, tarif)" // bakal dicek dulu
-                + "VALUES ('" +m.getId_Kendaraan()+ "', '" + m.getJenis_Helm()+ "','" + m.getJenis_Stang()+ "', '"
+        String sql = "INSERT INTO mobil(id_Kendaraan, kapasitas_Mobil, jenis, nama_Kendaraan, platNo, merk, cc, tarif)" // bakal dicek dulu
+                + "VALUES ('" +m.getId_Kendaraan()+ "', '" + m.getKapasitas_Mobil() + "', '"
                 + m.getJenis() + "', '" + m.getNama_Kendaraan() + "', '" + m.getPlatNo() + "', '" + m.getMerk() + 
                 "', '" + m.getCC() + "', '" + m.getTarif() + "')";
         
-        System.out.println("Adding Motor...");
+        System.out.println("Adding Mobil...");
         
         try{
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
-            System.out.println("Added " +result + " Motor");
+            System.out.println("Added " +result + " Mobil");
             statement.close();
         } catch (Exception e) {
-            System.out.println("Errror adding Motor...");
+            System.out.println("Errror adding Mobil...");
             System.out.println(e);
         }
         dbCon.closeConnection();
     }
     
-    public void updateMotor(Kendaraan_Motor m){
+    public void updateMobil(Kendaraan_Mobil m){
         con = dbCon.makeConnection();
         
-        String sql = "UPDATE motor SET jenis_Helm = '" + m.getJenis_Helm()
-                + "', jenis_Stang = '" + m.getJenis_Stang()
+        String sql = "UPDATE mobil SET kapasitas_Mobil = '" + m.getKapasitas_Mobil()
                 + "', jenis = '" + m.getJenis()
                 + "', nama_Kendaraan = '" + m.getNamaKendaraan()
                 + "', platNo = '" + m.getPlatNo()
@@ -52,28 +50,27 @@ public class motorDAO {
                 + "', cc = '" + m.getCC()
                 + "', tarif = '" + m.getTarif()
                 + "' WHERE id_Kendaraan = '" + m.getId_Kendaraan() + "'";
-        System.out.println("Editing Motor...");
+        System.out.println("Editing Mobil...");
         
         try {
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
-            System.out.println("Edited " + result + " Motor " + m.getId_Kendaraan());
+            System.out.println("Edited " + result + " Mobil " + m.getId_Kendaraan());
             statement.close();
         } catch (Exception e) {
-            System.out.println("Error Updating Motor...");
+            System.out.println("Error Updating Mobil...");
             System.out.println(e);
         }
         
         dbCon.closeConnection();
     }
     
-    public List<Kendaraan> showMotorBySearch(String query){
+    public List<Kendaraan> showMobilBySearch(String query){
         con = dbCon.makeConnection();
         
-        String sql = "SELECT m.* FROM motor as m WHERE (m.id_Kendaraan LIKE"
+        String sql = "SELECT m.* FROM mobil as m WHERE (m.id_Kendaraan LIKE"
                 + "'%" + query + "%'"
-                + "OR m.jenis_Helm LIKE '%" + query + "%'"
-                + "OR m.jenis_Stang LIKE '%" + query +"%'"
+                + "OR m.kapasitas_Mobil LIKE '%" + query + "%'"
                 + "OR m.jenis LIKE '%" + query + "%'"
                 + "OR m.nama_Kendaraan LIKE '%" + query + "%'"
                 + "OR m.platNo LIKE '%" + query + "%'"
@@ -81,7 +78,7 @@ public class motorDAO {
                 + "OR m.cc LIKE '%" + query + "%'"
                 + "OR m.tarif LIKE '%" + query + "%')";
 
-        System.out.println("Mengambil data Motor...");
+        System.out.println("Mengambil data Mobil...");
         
         List<Kendaraan> list = new ArrayList();
         
@@ -91,9 +88,8 @@ public class motorDAO {
             
             if (rs != null){
                 while (rs.next()){
-                        Kendaraan k = new Kendaraan_Motor(
-                            rs.getString("mt.jenis_Helm"),
-                            rs.getString("mt.jenis_Stang"),
+                        Kendaraan k = new Kendaraan_Mobil(
+                            rs.getString("mt.kapasitas_Mobil"),
                             rs.getString("mt.id_Kendaraan"),    
                             rs.getString("mt.jenis"),
                             rs.getString("mt.nama_Kendaraan"),
@@ -120,27 +116,27 @@ public class motorDAO {
         
         con = dbCon.makeConnection();
         
-        String sql = "DELETE FROM motor WHERE id_Kendaraan = " + id + "";
+        String sql = "DELETE FROM mobil WHERE id_Kendaraan = " + id + "";
                 
-        System.out.println("Deleting Motor...");
+        System.out.println("Deleting Mobil...");
         
         try {
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
-            System.out.println("Deleted " + result + " Motor" + id);
+            System.out.println("Deleted " + result + " Mobil" + id);
             statement.close();
         } catch (Exception e) {
-            System.out.println("Error Deleting Motor...");
+            System.out.println("Error Deleting Mobil...");
             System.out.println(e);
         }
         
         dbCon.closeConnection();
     }  
     
-    public List<Kendaraan> showMotor(){
+    public List<Kendaraan> showMobil(){
         con = dbCon.makeConnection();
         
-        String sql = "SELECT * FROM motor";
+        String sql = "SELECT * FROM mobil";
         System.out.println("Mengambil data Motor...");
         
         List<Kendaraan> list = new ArrayList();
@@ -151,9 +147,8 @@ public class motorDAO {
             
             if (rs != null){
                 while (rs.next()){
-                        Kendaraan k = new Kendaraan_Motor(
-                            rs.getString("mt.jenis_Helm"),
-                            rs.getString("mt.jenis_Stang"),
+                        Kendaraan k = new Kendaraan_Mobil(
+                            rs.getString("mt.kapasitas_Mobil"),
                             rs.getString("mt.id_Kendaraan"),    
                             rs.getString("mt.jenis"),
                             rs.getString("mt.nama_Kendaraan"),
@@ -176,5 +171,5 @@ public class motorDAO {
             dbCon.closeConnection();
             
             return list;
-        }       
+        }    
 }
