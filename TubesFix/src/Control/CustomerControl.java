fb08e369555b9301611671576744a77a3aac3c6b
thompluk <thomasdany38@gmail.com>
2022-06-08 12:23:10 +0700

    customer control dan table

---
+++ TubesFix/src/Control/CustomerControl.java
@@ -0,0 +1,39 @@
+
+package Control;
+
+import java.util.List;
+import dao.customerDAO;
+import java.util.List;
+import model.Customer;
+import java.sql.ResultSet;
+import table.TableCustomer;
+/**
+ *
+ * @author Thomas Dany H
+ */
+public class CustomerControl {
+    private customerDAO CDao = new customerDAO();
+    public void insertDataCustomer(Customer c){
+        CDao.insertCustomer(c);
+    }
+    
+    public List<Customer> showDataCustomer(String jenis){
+        List<Customer> dataCustomer = CDao.showCustomer();
+        return dataCustomer;
+    }
+    
+    public TableCustomer showCustomer(String query) {
+        
+        List<Customer> dataCustomer = CDao.showCustomerBySearch(query);
+        TableCustomer tableCustomer = new TableCustomer(dataCustomer);
+        
+        return tableCustomer;
+    }
+    
+    public void updateCustomer(Customer c){
+        CDao.updateCustomer(c);
+    }
+    public void deleteCustomer(String id){
+        CDao.deleteCustomer(id);
+    }
+}
---
+++ TubesFix/src/table/TableCustomer.java
@@ -0,0 +1,62 @@
+
+package table;
+
+import java.util.List;
+import model.Customer;
+import javax.swing.table.AbstractTableModel;
+/**
+ *
+ * @author Thomas Dany H
+ */
+public class TableCustomer extends AbstractTableModel{
+    private List<Customer> list;
+    
+    public TableCustomer(List<Customer> list){
+        this.list = list;
+    }
+    
+    @Override
+    public int getRowCount(){
+        return list.size();
+    }
+    
+    @Override
+    public int getColumnCount(){
+        return 4;
+    }
+    
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex){
+        switch(columnIndex){
+            case 0 :
+                return list.get(rowIndex).getId_Customer();
+            case 1:
+                return list.get(rowIndex).getNama_Customer();
+            case 2: 
+                return list.get(rowIndex).getAlamat_Customer();
+            case 3:
+                return list.get(rowIndex).getUmur_Customer();
+            case 4:
+                return list.get(rowIndex).getTelp_Customer();
+            default:
+                return null;
+        }
+    }
+    
+    public String getColumnName(int column){
+        switch(column){
+            case 0:
+                return "Id_Customer";
+            case 1:
+                return "Nama_Customer";
+            case 2:
+                return "Alamat_Customer";
+            case 3:
+                return "Umur_Customer";
+            case 4:
+                return "NomorTelp_Customer";   
+            default:
+                return null;
+        }
+    }
+}
