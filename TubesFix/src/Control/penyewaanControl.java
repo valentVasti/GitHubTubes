d6dae8257426aa6d5128b6d4b985d26dc7abb407
thompluk <thomasdany38@gmail.com>
2022-06-10 01:24:06 +0700

    peenyewaanDAO

---
+++ TubesFix/src/Control/penyewaanControl.java
@@ -0,0 +1,41 @@
+
+package Control;
+
+import java.util.List;
+import dao.penyewaanDAO;
+import model.Penyewaan;
+import model.Customer;
+import model.Guide;
+import model.Kendaraan;
+import model.Kendaraan_Mobil;
+import model.Kendaraan_Motor;
+import model.Transaksi;
+import java.sql.ResultSet;
+import table.TablePenyewaan;
+
+public class penyewaanControl {
+    private penyewaanDAO PDao = new penyewaanDAO(); 
+    public void insertDataPenyewaan(Penyewaan p){
+        PDao.insertPenyewaan(p);
+    }
+    
+    public List<Penyewaan> showDataPenyewaan(String jenis){
+        List<Penyewaan> dataPenyewaan = PDao.showPenyewaan();
+        return dataPenyewaan;
+    }
+    
+    public TablePenyewaan showPenyewaan(String query, String Jenis) {
+        
+        List<Penyewaan> dataPenyewaan = PDao.showListPenyewaan(query, Jenis);
+        TablePenyewaan tablePenyewaan = new TablePenyewaan(dataPenyewaan);
+        
+        return tablePenyewaan;
+    }
+    
+    public void updateCustomer(Penyewaan p){
+        PDao.updatePenyewaan(p);
+    }
+    public void deleteTransaksi(String id){
+        PDao.deletePenyewaan(id);
+    }
+}
