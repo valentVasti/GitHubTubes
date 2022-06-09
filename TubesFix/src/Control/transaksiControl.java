7db1968347f593484ed4c07b733318f045f1073a
thompluk <thomasdany38@gmail.com>
2022-06-10 00:56:15 +0700

    update transaksiDAO

--- TubesFix/src/Control/transaksiControl.java
+++ TubesFix/src/Control/transaksiControl.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Control;
 
-/**
- *
- * @author Thomas Dany H
- */
+import java.util.List;
+import dao.transaksiDAO;
+import model.Transaksi;
+import java.sql.ResultSet;
+import table.TableTransaksi;
+
 public class transaksiControl {
+    private transaksiDAO TDao = new transaksiDAO();
+    public void insertDataTransaksi(Transaksi t){
+        TDao.insertTransaksi(t);
+    }
     
+    public List<Transaksi> showDataTransaksi(String jenis){
+        List<Transaksi> dataTransaksi = TDao.showTransaksi();
+        return dataTransaksi;
+    }
+    
+    public TableTransaksi showTransaksi(String query) {
+        
+        List<Transaksi> dataTransaksi = TDao.showTransaksiBySearch(query);
+        TableTransaksi tableTransaksi = new TableTransaksi(dataTransaksi);
+        
+        return tableTransaksi;
+    }
+    
+    public void updateCustomer(Transaksi t){
+        TDao.updateTransaksi(t);
+    }
+    public void deleteTransaksi(String id){
+        TDao.deleteTransaksi(id);
+    }
 }
